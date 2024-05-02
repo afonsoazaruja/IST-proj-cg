5ff900d84c7c2955d73cb8dd3b75b4c640d9bf90
Tiago Borbinha <tiago.borbinha@tecnico.ulisboa.pt>
2024-05-02 11:02:43 +0100

    Pós-Aula 1

--- js/main-script.js
+++ js/main-script.js
@@ -7,45 +7,59 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-var camera, scene, renderer;
+var cameras=new Array(), scene, renderer;
 
-var geometry, material, mesh;
+var geometry, material1, material2, mesh;
 
 var base, crane;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createBase(x, y, z) {
-    'use strict';
-    base = new THREE.BoxGeometry(5, 5, 5);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
-}
-
 function createScene(){
     'use strict';
-
+    const color = new THREE.Color( 'skyblue' );
     scene = new THREE.Scene();
+    scene.background = color;
     scene.add(new THREE.AxesHelper(10));
+    createBase();
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-function createCamera() {
+function createPersepectiveCamera(id,x,y,z) {
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
+    var cam;
+    cam = new THREE.PerspectiveCamera(70,
                                          window.innerWidth / window.innerHeight,
                                          1,
                                          1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
+    cam.position.x = x;
+    cam.position.y = y;
+    cam.position.z = z;
+    cam.lookAt(scene.position);
+
+    cameras[id] = cam;
+    cam.lookAt(scene.position);
 }
 
+function createOrthographicCamera(id,x,y,z) {
+    'use strict';
+    cameras[id] = new THREE.OrthographicCamera(window.innerWidth / -16, window.innerWidth / 16, window.innerHeight / 16, window.innerHeight / -16, 1, 1000);
+    cameras[id].position.x = x;
+    cameras[id].position.y = y;
+    cameras[id].position.z = z;
+    cameras[id].lookAt(scene.position);
+
+    new THREE.cameras[id]
+}
+
+function createCameras(){
+    for(var i=0;i<6;i++){
+        createPersepectiveCamera(i,17,17,17);
+    }
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -54,6 +68,54 @@ function createCamera() {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+/* Function that creates a cube and positions it in the referencial */
+function addCube(obj, x, y, z, ref_x, ref_y, ref_z, material) {
+    'use strict';
+    
+    geometry = new THREE.BoxGeometry(x, y, z);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(ref_x, ref_y, ref_z);
+    obj.add(mesh);
+}
+
+/* Function that creates a cilinder and positions it in the referencial */
+function addCylinder(obj, rt, rb, h, ref_x, ref_y, ref_z) {
+    'use strict';
+    
+    geometry = new THREE.CylinderGeometry(rt, rb, h, rs);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(ref_x, ref_y, ref_z);
+    obj.add(mesh);
+}
+
+/* Function that creates a Tetrahedron and positions it in the referencial */
+function addTetrahedron(obj, r, d, ref_x, ref_y, ref_z, material) {
+    'use strict';
+    
+    geometry = new THREE.TetrahedronGeometry(r, d);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(ref_x, ref_y, ref_z);
+    obj.add(mesh);
+}
+
+function createBase() {
+    'use strict';
+
+    var base = new THREE.Object3D();
+
+    material1 = new THREE.MeshBasicMaterial({ color: 0xEEAD2D, wireframe: false });
+    material2 = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false });
+    
+    addCube(base, 2, 1, 2, 0, 0, 0, material2); // add base
+    addCube(base, 1, 10, 1, 0, 5, 0, material1); // add tower
+
+    scene.add(base);
+
+    base.position.x = 0;
+    base.position.y = 0;
+    base.position.z = 0;
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -84,7 +146,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-    renderer.render(scene, camera);
+    renderer.render(scene, cameras[0]);
 }
 
 ////////////////////////////////
@@ -99,9 +161,7 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createCamera();
-
-    render();
+    createCameras();
 }
 
 /////////////////////
@@ -128,6 +188,55 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    /*switch (e.keyCode) {
+    case 49: //'1'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                node.material.wireframe = !node.material.wireframe;
+            }
+        });
+        break;
+
+    case 50: //'2'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                node.material.wireframe = !node.material.wireframe;
+            }
+        });
+        break;
+
+    case 51: //'3'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxesHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+
+    case 52: //'4'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxesHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+
+    case 53: //'5'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxesHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+    case 53: //'6'
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxesHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+
+    }*/
 }
 
 ///////////////////////
